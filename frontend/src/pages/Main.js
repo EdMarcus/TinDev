dd01f55b744636794879d6c449a7bbce36350ccb
Édlon <edlondesa@gmail.com>
2019-08-07 20:31:46 -0300

    Aula 03 - FrontEnd da aplicação

--- frontend/src/pages/Main.js
+++ frontend/src/pages/Main.js
@@ -66,7 +66,7 @@ export default function Main({match}){
                 ))}
                 </ul>
             ) : (
-                <div className="empty">Não há mais devs! :/</div>
+                <div className="empty">Acabaram os devs! :/</div>
             )}
         </div>
     );
